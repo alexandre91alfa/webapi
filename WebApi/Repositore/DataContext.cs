798c607f91e085ffcc9dc22590760a503000ee46
alexandre <alexandre.alfa@yahoo.com>
2019-11-14 10:22:01 -0300

    include repositore

---
+++ WebApi/Repositore/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Repositore
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base (options) {}
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
